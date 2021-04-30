9ab3e67c474b606d7cd714ab205f072924c85bc6
JessicaJuliet <jessica.akomfrah@gmail.com>
2021-04-30 11:20:55 +0000

    Add find and update functions to mongo_project.py

--- mongo.py
+++ mongo.py
@@ -4,25 +4,23 @@ if os.path.exists("env.py"):
     import env
 
 
-
 MONGO_URI = os.environ.get("MONGO_URI")
 DATABASE = "myFirstDB"
 COLLECTION = "celebrities"
+
 
 def mongo_connect(url):
     try:
         conn = pymongo.MongoClient(url)
         print("Mongo is connected")
         return conn
-    except pymongo.errors.ConnectionFailure as en:
+    except pymongo.errors.ConnectionFailure as e:
         print("Could not connect to MongoDB: %s") % e
 
 
 conn = mongo_connect(MONGO_URI)
 
 coll = conn[DATABASE][COLLECTION]
-
-new_doc = {"first": "douglas", "last": "adams"}
 
 documents = coll.find()
 
